150509da42385017947582eede7cc2fcc77c0f6f
Shawn Liu <xliu@siocsxliu.local>
2017-11-15 12:06:09 -0500

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
 import {Provider} from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import configureStore from './store/configure-store';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.scss';
+
+import App from './App';
 
 const store = configureStore();
 
